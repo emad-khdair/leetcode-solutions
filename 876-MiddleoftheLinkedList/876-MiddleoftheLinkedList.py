30748fb25d523c84a1679ba81c57583ec6a1aec0
emad-khdair <mohammed.khdair@outlook.com>
2025-08-16 21:46:00 +0400

    Create 876-MiddleoftheLinkedList

---
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.py
@@ -0,0 +1,15 @@
+# Last updated: 8/16/2025, 9:45:59 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
