29fcb4710d6c9d8332c1e4308a01e9ff0588693c
emad-khdair <mohammed.khdair@outlook.com>
2025-08-30 15:52:24 +0400

    File updated at 8/30/2025, 3:52:24 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,14 +1,12 @@
-# Last updated: 8/30/2025, 3:37:02 PM
+# Last updated: 8/30/2025, 3:52:24 PM
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash_map = defaultdict(list)
+        mp = defaultdict(list)
 
         for word in strs:
             count = [0] * 26
-            
             for char in word:
                 count[ord(char) - ord('a')] += 1
+            mp[tuple(count)].append(word)
 
-            hash_map[tuple(count)].append(word)
-
-        return list(hash_map.values())
+        return list(mp.values())
