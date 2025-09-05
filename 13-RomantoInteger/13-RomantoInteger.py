f4dd217558b3b7204fa06fe1005089991af1ae92
emad-khdair <mohammed.khdair@outlook.com>
2025-09-05 20:08:19 +0400

    Create 13-RomantoInteger

---
+++ 13-RomantoInteger/13-RomantoInteger.py
@@ -0,0 +1,25 @@
+# Last updated: 9/5/2025, 8:08:19 PM
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+
+        i = 0
+        count = 0
+
+        while i < len(s):
+            if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
+                count += roman_map[s[i + 1]] - roman_map[s[i]]
+                i += 2
+            else:
+                count += roman_map[s[i]]
+                i += 1
+
+        return count
