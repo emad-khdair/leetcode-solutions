94123f95c35dc77d43f5509be2a2293114b35b97
emad-khdair <mohammed.khdair@outlook.com>
2025-09-15 20:10:15 +0400

    File updated at 9/15/2025, 8:10:15 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,4 +1,4 @@
-# Last updated: 9/2/2025, 9:33:21 PM
+# Last updated: 9/15/2025, 8:10:15 PM
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_map = {}
@@ -7,6 +7,4 @@ class Solution:
             complement = target - num
             if complement in num_map:
                 return [num_map[complement], index]
-            num_map[num] = index
-
-    
+            num_map[num] = index 
