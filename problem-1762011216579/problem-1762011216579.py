afb1741b91b4eb0ba927779b15995dbeee754694
emad-khdair <mohammed.khdair@outlook.com>
2025-11-01 19:33:38 +0400

    Create

---
+++ problem-1762011216579/problem-1762011216579.py
@@ -0,0 +1,18 @@
+# Last updated: 11/1/2025, 7:33:36 PM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+            i += 1
+            j -= 1
+
+        return True
