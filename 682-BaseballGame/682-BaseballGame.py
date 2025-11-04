2612c9525f8cda79a4ecb21d24f4f28ee8eb9c80
emad-khdair <mohammed.khdair@outlook.com>
2025-11-04 08:09:44 +0400

    File updated at 11/4/2025, 8:09:44 AM

--- 682-BaseballGame/682-BaseballGame.py
+++ 682-BaseballGame/682-BaseballGame.py
@@ -1,14 +1,15 @@
-# Last updated: 11/4/2025, 7:49:14 AM
+# Last updated: 11/4/2025, 8:09:44 AM
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
+
         for op in operations:
-            if op == 'C':
-                stack.pop()
+            if op == '+':
+                stack.append(stack[-1] + stack[-2])
             elif op == 'D':
                 stack.append(stack[-1] * 2)
-            elif op == '+':
-                stack.append(stack[-1] + stack[-2])
+            elif op == 'C':
+                stack.pop()
             else:
                 stack.append(int(op))
 
