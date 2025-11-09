fd8c69f10c4cd7285e0f05e970a8e7c4c392e414
emad-khdair <mohammed.khdair@outlook.com>
2025-11-09 19:49:08 +0400

    File updated at 11/9/2025, 7:49:07 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,11 +1,15 @@
-# Last updated: 11/9/2025, 7:18:10 PM
+# Last updated: 11/9/2025, 7:49:07 PM
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_map = defaultdict(list)
 
-        for word in strs:
-            a_key = ''.join(sorted(word))
+        for s in strs:
+            count = [0] * 26 
 
-            anagram_map[a_key].append(word)
+            for l in s:
+                count[ord(l) - ord('a')] += 1
+
+            anagram_map[tuple(count)].append(s)
 
         return list(anagram_map.values())
+            
