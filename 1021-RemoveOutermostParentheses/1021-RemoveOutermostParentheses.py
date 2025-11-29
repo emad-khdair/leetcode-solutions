b1228d26f116c4ca0bdc59c73487864af6c1f46e
emad-khdair <mohammed.khdair@outlook.com>
2025-11-29 23:10:47 +0400

    File updated at 11/29/2025, 11:10:48 PM

--- 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.py
+++ 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.py
@@ -1,17 +1,17 @@
-# Last updated: 11/12/2025, 10:02:18 AM
-class Solution:
-    def removeOuterParentheses(self, s: str) -> str:
-        result = []
-        depth = 0
-
-        for char in s:
-            if char == '(':
-                if depth > 0:
-                    result.append(char)
-                depth += 1
-            else:
-                depth -= 1
-                if depth > 0:
-                    result.append(char)
-
-        return "".join(result)
+# Last updated: 11/29/2025, 11:10:48 PM
+1class Solution:
+2    def removeOuterParentheses(self, s: str) -> str:
+3        result = []
+4        depth = 0
+5
+6        for char in s:
+7            if char == '(':
+8                if depth > 0:
+9                    result.append(char)
+10                depth += 1
+11            else:
+12                depth -= 1
+13                if depth > 0:
+14                    result.append(char)
+15
+16        return "".join(result)
